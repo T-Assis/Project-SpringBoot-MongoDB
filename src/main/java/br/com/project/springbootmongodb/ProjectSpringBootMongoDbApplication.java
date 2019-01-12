d3a0efd516e19b69d3ecf0b46ce227af1ae696cd
Thales Lima <thaleslima-19@hotmail.com>
2019-01-11 22:06:02 -0200

    Project created

---
+++ src/main/java/br/com/project/springbootmongodb/ProjectSpringBootMongoDbApplication.java
@@ -0,0 +1,14 @@
+package br.com.project.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootMongoDbApplication.class, args);
+	}
+
+}
+
