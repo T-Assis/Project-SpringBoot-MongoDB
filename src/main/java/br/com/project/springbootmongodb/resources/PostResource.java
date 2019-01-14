7466e0d1ae14ce96f273fbcbc9a1b4291642b0b1
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-01-14 15:46:39 -0200

    Getting a post by id

---
+++ src/main/java/br/com/project/springbootmongodb/resources/PostResource.java
@@ -0,0 +1,28 @@
+package br.com.project.springbootmongodb.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.project.springbootmongodb.domain.Post;
+import br.com.project.springbootmongodb.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) // @GetMapping
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+	
+	
+
+}
---
+++ src/main/java/br/com/project/springbootmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package br.com.project.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.project.springbootmongodb.domain.Post;
+import br.com.project.springbootmongodb.repository.PostRepository;
+import br.com.project.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	
+	
+}
